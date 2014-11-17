d896eb191e9703d5f02fffca8b138a0a650c2fb6
swapsharma <mradul.master@gmail.com>
2014-11-17 22:57:26 +0530

    Added link to Browse Room

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 	else echo "Hey ".$_COOKIE['user']."!";
 	?>
 	<a href = "reservation_form1.php">Reservation</a>
-	<a href = "">Room Availability</a>
+	<a href = "brwsrm.php">Room Availability</a>
 	<a href = "">Upcoming Events</a>
 	<a href = "">Contact</a>
 	<a href = "">FAQ</a>
