03fcdd84aeca25c12b76938ef52ec22d94d61878
swapsharma <mradul.master@gmail.com>
2014-11-18 21:38:20 +0530

    Removed minor bugs

--- reservation_form4.php
+++ reservation_form4.php
@@ -126,5 +126,9 @@
             <a href = "index.php" class = "btn btn-default">Cancel</a>
         </div>
         </form>
+
+        <!--JavaScript Files-->
+        <script src="dist/js/jquery.min.js"></script>
+        <script src="dist/js/bootstrap.min.js"></script>
     </body>
 </html>
