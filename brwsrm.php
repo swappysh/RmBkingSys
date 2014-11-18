35cb5064d24dc741d32e50c4315fe134cd9c6698
swapsharma <mradul.master@gmail.com>
2014-11-19 04:05:35 +0530

    Fixed bug

--- brwsrm.php
+++ brwsrm.php
@@ -118,6 +118,9 @@
             </table>
         </div>
 
+    <!--Javascript files-->
+    <script src="dist/js/jquery.min.js"></script>
+    <script src="dist/js/bootstrap.min.js"></script>
 	</body>
 
 </html>
