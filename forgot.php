b49f0e14163907d146c7adce7ec88c73c0dd8a7b
swapsharma <mradul.master@gmail.com>
2014-11-26 05:11:56 +0530

    Completed form

--- forgot.php
+++ forgot.php
@@ -1,23 +1,83 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <title>Template</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="dist/css/bootstrap.min.css">
-</head>
+    <html>
+    <head>
+        <title>Template</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    </head>
 
     <body>
-        <form name = "Forgot Form"  onsubmit="return validateForm()" method = "post"  action = '' >
-            <label>eMail ID:<input type = "email" name = "email" /></label><br>
-            <label>Mobile Number:<input type="text" name="mob_num" /></label><br>
-            <input type="button" name="sumbit" value="Submit"/>
-        </form>
+        <!--Navigation Bar-->
+        <nav class = "navbar navbar-default">
+            <div class = "container-fluid">
+                <div class = "navbar-header">
+                    <a class = "navbar-brand" href="index.php">RmBkingSys</a>
+                </div>
+                <div>
+                    <ul class = "nav navbar-nav">
+                        <li><a href = "index.php">Home</a></li>
+                        <li><a href = "reservation_form1.php">Reservation</a></li>
+                        <li><a href = "brwsrm.php">Room Availability</a></li>
+                        <li><a href = "upmingents.php">Upcoming Events</a></li>
+                        <li><a href = "index.php#cntctus">Contact</a></li>
+                        <li><a href = "index.php#faq">FAQ</a></li>
+                        <li><a href = "index.php#about">About Us</a></li>
+                    </ul>
+                    <ul class = "nav navbar-nav navbar-right">
+                        <?php
+                        if (!isset( $_COOKIE['root_user'] )) {
+                            echo "<li>"
+                                ."<a href = \"root_user/rtloginform.php\">Admin Login</a></li>";
+                        }
+                        else echo "<li class=\"dropdown\">"
+                                    ."<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">Admin <span class=\"caret\"></span></a>"
+                                    ."<ul class=\"dropdown-menu\">"
+                                            ."<li><a href=\"root_user/adrm.php\">Add Room</a></li>"
+                                            ."<li><a href=\"root_user/delrm.php\">Delete Room</a></li>"
+                                            ."<li><a href=\"root_user/rmresvion.php\">Cancel Reservation</a></li>"
+                                            ."<li><a href = \"root_user/rmuser.php\">Remove User</a></li>"
+                                            ."<li><a href=\"root_user/rtlogout.php\">Log Out</a></li>"
+                                        ."</ul>"
+                                    ."</li>";
+                    ?>
 
-        <a href="index.php" >
-        	<div class = "button">
-        		<button name="cancel" value="cancel">Cancel</button>
-        	</div>
-        </a>
+                    <?php
+                        if (!isset( $_COOKIE['user'] )) {
+                            echo "<li class = \"active\">"
+                                ."<a href = \"loginform.php\">Login</a></li>";
+                        }
+                        else echo "<li class=\"dropdown\">"
+                                    ."<a class=\"dropdown-toggle\" data-toggle=\"dropdown\" href=\"#\">My Account <span class=\"caret\"></span></a>"
+                                    ."<ul class=\"dropdown-menu\">"
+                                            ."<li><a href=\"My_Account/shwple.php\">Show Profile</a></li>"
+                                            ."<li><a href=\"My_Account/edit.php\">Edit</a></li>"
+                                            ."<li><a href=\"My_Account/bkinghtry.php\">Booking History</a></li>"
+                                            ."<li><a href=\"My_Account/logout.php\">Log Out</a></li>"
+                                        ."</ul>"
+                                    ."</li>";
+                    ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!--Form-->
+        <div class = "container">
+            <form role = "form" name = "Forgot Form"  onsubmit="return validateForm()" method = "post"  action = '' >
+                    <div class = "form-group">
+                        <input type = "email" name = "Email" class = "form-control" placeholder = "Email" maxlength = 50 />
+                    </div>
+                    <div class = "form-group">
+                        <input type = "text" name = "mob_num" class = "form-control" placeholder = "Mobile Number" maxlength = 50 />
+                    </div>
+                    <div>
+                        <input type="submit" class = "btn btn-default" name="submit" value="Submit"/>
+                    </div>
+            </form>
+        </div>
+
+        <script src="dist/js/jquery.min.js"></script>
+        <script src="dist/js/bootstrap.min.js"></script>
     </body>
 </html>
